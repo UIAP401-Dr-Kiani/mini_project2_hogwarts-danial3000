f48422c6c56a8e1bb95c95f0a5673ccbec274b08
danial3000 <danstinhafman@gmail.com>
2023-05-03 02:02:05 +0430

    adding from file to instance of Human class

--- hagwartz_console/hagwartz/Class1.cs
+++ hagwartz_console/hagwartz/Class1.cs
@@ -8,24 +8,43 @@ namespace hagwartz
 {
     public class Human
     {
-        public string name ;
-        public string lastname;
-        public int birthyear;
-        public bool gender;
+        public string name= "";
+        public string lastname = "";
+        public int birthyear = -1 ;
+        public bool gender = false ;
         // man = false //  woman = true
-        public string fathersname;
-        public int username;
-        public int password;
+        public string fathersname = "";
+        public string username = "";
+        public string password = "";
         public enum kind
         {
-            muggle_blood , pure_blood , half_blood
+            muggle_blood, pure_blood, half_blood
         }
+        public kind kindof = kind.muggle_blood ;
+
+    }
+
+    public class Acceptable : Human
+    {
+        public string schedule ;
+        public enum pet
+        {
+            rat , cat , owl
+        }
+        public string group ;
+        bool suitcase ;
+        public enum job
+        {
+            student , teacher
+        }
+        public string[] mails;
+
     }
     
     public class Team
     {
         public int score;
-        public enum type
+        public enum group
         {
             slytherin , ravenclaw , gryfinndor , hufflepuff
         }
@@ -33,33 +52,78 @@ namespace hagwartz
         public string[] quideech_members;
     }
 
-    public class Student
+    public class Student : Acceptable
     {
         public int passed_units;
         public int term;
         public int dormitory_number;
     }
 
-    public class Teacher
+    public class Teacher : Acceptable
     {
         public bool concurrent_teaching;
     }
-    /*public class Dumbeldor
+    public class Dumbeldor : Acceptable
     {
-
-    }*/
+        Dormitory[] list_of_dormitory;
+        static private Dumbeldor dum;
+        private Dumbeldor()
+        {
+        }
+        public static Dumbeldor geter()
+        {
+            if( dum == null )
+            {
+                dum = new Dumbeldor();
+            }
+            return dum;
+        }
+    }
     public class Lessons
     {
         public DateTime time;
         public int numberofstudents;
-        public string name;
+        public string name ;
         public int presentationterm;
         public int capacity;
     }
 
     public class Plant
     {
-        public string name;
+        public string name ;
         public int number;
+    }
+    public class Chemistary : Lessons
+    {
+        public string[] name_of_materials;
+    }
+    public class Magic : Lessons
+    {
+        public string[] Charms;
+    }
+    public class Sport : Lessons
+    {
+        public string sport_kind ;
+    }
+    public class Botany : Lessons
+    {
+        public string[] listofplant1;
+        public string[] listofplant2;
+        public string[] listofplant3;
+        public string[] listofplant4;
+    }
+    public class Dormitory
+    {
+        public string group;
+        public int floor;
+        public int room;
+        public int bed;
+        public bool gender;
+        Dormitory(int frb)
+        {
+            this.floor = (frb / 100) % 5;
+            this.room = (frb / 10) % 10;
+            this.bed = (frb % 10);
+        }
     }
 }
--- hagwartz_console/hagwartz/Program.cs
+++ hagwartz_console/hagwartz/Program.cs
@@ -1,10 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+
 namespace hagwartz
 {
     class program
     {
-        static void main(String[] args) 
+        static void Main(String[] args) 
         {
+            int sefr = 0;
+            string[] human1 = new string [100];
+
+            using (StreamReader file = new StreamReader(@"D:\file.txt"))
+            {
+                string ln;
+                while((ln = file.ReadLine()) != null)
+                {
+                    human1 = ln.Split("\t").ToArray<string>();
+                }
+                file.Close();
+            }
+            Human[] human = new Human[100];
+            // instace of Human class
+            for (int i = 0; i < human1.Length; i++)
+                human[i] = new Human();
+            // complete instances of Human class
+            for (int i = 0; i < human1.Length; i++)
+            {
+                if (i % 8 == 0)
+                    human[i / 8].name = human1[i];
+                else if(i % 8 == 1)
+                    human[i/8].lastname = human1[i];
+                else if (i % 8 == 2)
+                    human[i/8 ].birthyear = Convert.ToInt32(human1[i]);
+                else if (i % 8 == 3)
+                {
+                    if (human1[i % 8] == "man")
+                        human[i / 8].gender = false;
+                    else if (human1[i % 8] == "woman")
+                        human[i / 8].gender = true;
+                }
+                else if (i % 8 == 4)
+                    human[i/8].fathersname = human1[i];
+                else if(i % 8 == 5)
+                    human[i/9].username = human1[i];
+                else if (i % 8 == 6)
+                    human[i / 8].password = human1[i];
+                else if (i % 8 == 7)
+                {
+                    if (human1[i % 8] == "muggle_blood")
+                        human[i / 8].kindof = Human.kind.muggle_blood;
+                    else if (human1[i % 8] == "pure_blood")
+                        human[i / 8].kindof = Human.kind.pure_blood;
+                    else if (human1[i % 8] == "half_blood")
+                        human[i / 8].kindof = Human.kind.muggle_blood;
+                }
+            }
+            // dar ghesmat kind moshkel darad ke bayad bartaraf shavad !!!!!!!!!!!
+
+
+
+
+            Dumbeldor dumb = Dumbeldor.geter();
+
 
         }
     }
